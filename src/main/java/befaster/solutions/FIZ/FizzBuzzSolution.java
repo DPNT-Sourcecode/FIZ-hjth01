b2690b1de5d82bc178734acf91956e4439dfb19a
sbx_user1051 <sbx_user1051@169.254.88.121>
2019-04-11 18:00:02 +0000

    Thu Apr 11 18:00:02 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -2,16 +2,28 @@ package befaster.solutions.FIZ;
 
 public class FizzBuzzSolution {
 
-    public String fizzBuzz(Integer number) {
-    	if ((number % 3 == 0 || String.valueOf(number).contains("3")) && (number % 5 == 0  || String.valueOf(number).contains("5"))) {
+	public String fizzBuzz(Integer number) {
+    	String numberStr = String.valueOf(number);
+    	if (number > 10 && containsIdenticalDigits(numberStr)) {
+    		return "deluxe";
+    	} else if ((number % 3 == 0 || numberStr.contains("3")) && (number % 5 == 0  || numberStr.contains("5"))) {
     		return "fizz buzz";
-    	} else if (number % 3 == 0 || String.valueOf(number).contains("3")) {
+    	} else if (number % 3 == 0 || numberStr.contains("3")) {
     		return "fizz";
-    	} else if (number % 5 == 0 || String.valueOf(number).contains("5")) {
+    	} else if (number % 5 == 0 || numberStr.contains("5")) {
     		return "buzz";
     	} else {
     		return String.valueOf(number);
     	}
     }
 
+	public static boolean containsIdenticalDigits(String numberStr) {
+		for (char c : numberStr.toCharArray()) {
+			if (c != numberStr.charAt(0)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
+
