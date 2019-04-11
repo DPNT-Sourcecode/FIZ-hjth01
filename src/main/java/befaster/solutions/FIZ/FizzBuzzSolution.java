da8e1d4f23939b79f84c21eb09fb4e18bb735b6b
sbx_user1051 <sbx_user1051@169.254.88.121>
2019-04-11 18:03:02 +0000

    Thu Apr 11 18:03:02 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,19 +3,23 @@ package befaster.solutions.FIZ;
 public class FizzBuzzSolution {
 
 	public String fizzBuzz(Integer number) {
-    	String numberStr = String.valueOf(number);
-    	if (number > 10 && containsIdenticalDigits(numberStr)) {
-    		return "deluxe";
-    	} else if ((number % 3 == 0 || numberStr.contains("3")) && (number % 5 == 0  || numberStr.contains("5"))) {
-    		return "fizz buzz";
-    	} else if (number % 3 == 0 || numberStr.contains("3")) {
-    		return "fizz";
-    	} else if (number % 5 == 0 || numberStr.contains("5")) {
-    		return "buzz";
-    	} else {
-    		return String.valueOf(number);
-    	}
-    }
+		String numberStr = String.valueOf(number);
+		boolean identicalDigits = containsIdenticalDigits(numberStr);
+		if ((number % 3 == 0 || numberStr.contains("3")) && (number % 5 == 0 || numberStr.contains("5"))
+				&& (number > 10 && identicalDigits)) {
+			return "fizz buzz deluxe";
+		} else if (identicalDigits) {
+			return "deluxe";
+		} else if ((number % 3 == 0 || numberStr.contains("3")) && (number % 5 == 0 || numberStr.contains("5"))) {
+			return "fizz buzz";
+		} else if (number % 3 == 0 || numberStr.contains("3")) {
+			return "fizz";
+		} else if (number % 5 == 0 || numberStr.contains("5")) {
+			return "buzz";
+		} else {
+			return String.valueOf(number);
+		}
+	}
 
 	public static boolean containsIdenticalDigits(String numberStr) {
 		for (char c : numberStr.toCharArray()) {
@@ -26,4 +30,5 @@ public class FizzBuzzSolution {
 		return true;
 	}
 }
+
 
---
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -0,0 +1,23 @@
+package befaster.solutions.FIZ;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzSolutionTest {
+    private FizzBuzzSolution fiz;
+
+    @Before
+    public void setUp() {
+
+    	fiz = new FizzBuzzSolution();
+    }
+
+    @Test
+    public void checkIdentical() {
+        assertThat(fiz.fizzBuzz(333), equalTo("deluxe"));
+    	
+    }
+}
